e78e4f96ff798e94b5f896978c85a9897241b4bd
joshdanseco-bit <joshdanseco@gmail.com>
2026-06-25 16:35:19 +0800

    Add files via upload

---
+++ hello_cpp102.py
@@ -0,0 +1,6 @@
+print("Hello,CPP102!")
+print("I am learning python, Git, and Github")
+
+print("Hello, CPP102!")
+print("My name is Josh Wilfried Danseco, Section CPE1-B.")
+print("I am learning Python, Git, and GitHub.")
