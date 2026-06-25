bd7cf936049b84de2eecad3334188691365c036d
joshdanseco-bit <joshdanseco@gmail.com>
2026-06-25 16:36:02 +0800

    Add files via upload

---
+++ new program.py
@@ -0,0 +1,12 @@
+print("Hello, World")
+name = "Josh Wilfried Danseco"
+age = 19
+university = "University of Cabuyao"
+section = "CPE-B"
+
+# Displaying the information on the screen
+print("=== Student Profile ===")
+print("Name:", name)
+print("Age:", age)
+print("University:", university)
+print("Course & Section:", section)
